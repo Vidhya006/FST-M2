c960be373a565d559d841830c3cb34ce996db91e
Vidhya006 <43105394+Vidhya006@users.noreply.github.com>
2022-11-02 22:00:19 +0530

    Add files via upload

---
+++ junit/Activity2.java
@@ -0,0 +1,26 @@
+package activities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class Activity2 {
+
+    @Test
+    void notEnoughFunds(){
+
+        BankAccount account = new BankAccount(9);
+        assertThrows(NotEnoughFundsException.class, () -> account.withdraw(11),"Balance must be greater than amount of withdrawal");
+    }
+
+    @Test
+    void enoughFunds(){
+        BankAccount account = new BankAccount(100);
+        assertDoesNotThrow(()->account.withdraw(50));
+
+    }
+
+
+
+}
